c8b42eeb0e95c8ecb62dadc188c85be47acd0453
Ravikiran <ravikirannagure@gmail.com>
2025-04-23 08:34:00 +0530

    triangle program chnges

--- src/test/java/IntegerProgram/DiffereentTriangle.java
+++ src/test/java/IntegerProgram/DiffereentTriangle.java
@@ -3,25 +3,121 @@ package IntegerProgram;
 public class DiffereentTriangle {
 	
 	    public static void main(String[] args) {
-	        int n = 5;
+	    	int n = 5;
+			/* 
+	*
+	**
+	***
+	****
+	*****
+		*/
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
 
-	        // Right-Angled Triangle
-	        System.out.println("Right-Angled Triangle:");
-	        for (int i = 1; i <= n; i++) {
-	            System.out.println("*".repeat(i));
-	        }
+			
+			
+			/* 
+	    *
+	   **
+	  ***
+	 ****
+	*****
+		*/
+			
+		  for (int i = 1; i <= 5; i++) {
+				for (int k = n - i; k >= 1; k--) {
+					System.out.print(" ");
+				}
+				for (int j = 1; j <= i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			
+			
+			/* 
+		* 
+	   * * 
+	  * * * 
+	 * * * * 
+	* * * * * 
+			*/
+			
+			
+		  for (int i = 1; i <= n; i++) {
+				for (int j = n - i; j >= 1; j--) {
+					System.out.print(" ");
+				}
+				for (int k = 1; k <= i; k++) {
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+			
+			/* 
+	*****
+	 ****
+	  ***
+	   **
+	    * 
+			*/
+			
+			
+			
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j < i; j++) {
+					System.out.print(" ");
+				}
+				for (int k = i; k <= n; k++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			
+			
+			/* 
+	* * * * * 
+	 * * * * 
+	  * * * 
+	   * * 
+	    * 
+			*/
+			
+			
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j < i; j++) {
+					System.out.print(" ");
+				}
+				for (int k = i; k <= n; k++) {
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+			
+			/* 
+	*
+	**
+	* *
+	*  *
+	*****
+			*/
+			
+			
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= i; j++) {
+					if ((j == 1) || (j == i) || (i == n)) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
 
-	        // Inverted Right-Angled Triangle
-	        System.out.println("\nInverted Right-Angled Triangle:");
-	        for (int i = n; i >= 1; i--) {
-	            System.out.println("*".repeat(i));
-	        }
-
-	        // Equilateral Triangle
-	        System.out.println("\nEquilateral Triangle:");
-	        for (int i = 1; i <= n; i++) {
-	            System.out.println(" ".repeat(n - i) + "* ".repeat(i));
-	        }
+				}
+				System.out.println();
+			}
 	       
 	    }
 	}
